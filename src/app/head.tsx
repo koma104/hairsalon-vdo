c6d38f65e309bef83a82449bc11c281fb36096e8
Toshihito Komatsu <35862605+koma104@users.noreply.github.com>
2025-07-07 15:27:58 +0900

    feat: faviconを追加

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" type="image/png" sizes="256x256" href="/favicon.png" />
+      <link rel="apple-touch-icon" sizes="256x256" href="/favicon.png" />
+    </>
+  )
+} 
